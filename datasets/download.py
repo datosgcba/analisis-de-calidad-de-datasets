72532f81ee2af595ca6c8fff03e802a7e03ff7f3
Ignacio Heredia <ignacio.nh@gmail.com>
2018-12-15 18:02:04 -0300

    Added some zip management but not sur if works. Site is down

--- datasets/download.py
+++ datasets/download.py
@@ -3,6 +3,7 @@ from scrapy.http import Request
 import json
 import os
 from scrapy.http import FormRequest
+import zipfile
 
 dirname = os.path.dirname(__file__)
 manifest_path = os.path.join(dirname, 'manifest.json')
@@ -16,29 +17,35 @@ def scoped_lambda(func, param):
 
 class DatasetsSpider(scrapy.Spider):
     name = 'datasetsspider'
+    authorizaton_bearer = ''
 
     def start_requests(self):
         return [FormRequest("https://data.buenosaires.gob.ar/api/clients/tokens",
                             formdata={'consumer': 'frontend', 'consumerId': "7fb5addf-5263-4a28-91dd-8f9bbc44ab16"},
-                            callback=self.query_datasets)]
+                            callback=self.save_authorizaton_bearer)]
 
-    def query_datasets(self, response):
-        authorizaton_bearer = json.loads(response.body)['data']
-        for dataset in manifest:
-            if dataset['slug']:
-                slug = dataset['slug']
-                url = 'https://data.buenosaires.gob.ar/api/datasets?fields=id&slug=%s' % slug
-                yield Request(
-                    url=url,
-                    callback=scoped_lambda(self.get_download_url, slug),
-                    headers={'Authorization': 'Bearer %s' % authorizaton_bearer}
-                )
+    def save_authorizaton_bearer(self, response):
+        self.authorizaton_bearer = json.loads(response.body)['data']
+        self.query_next_dataset()
+
+    def query_next_dataset(self):
+        if len(manifest) == 0:
+            return
+        dataset = manifest.pop()
+        slug = dataset['slug']
+        url = 'https://data.buenosaires.gob.ar/api/datasets?fields=id&slug=%s' % slug
+        yield Request(
+            url=url,
+            callback=scoped_lambda(self.get_download_url, slug),
+            headers={'Authorization': 'Bearer %s' % self.authorizaton_bearer}
+        )
 
     def get_download_url(self, response, slug):
         response_data = json.loads(response.body)['data']
         if len(response_data) == 0:
             return
         dataset_id = response_data[0]['id']
+        self.logger.info('Got id %s for %s', dataset_id, slug)
         url = 'https://data.buenosaires.gob.ar/api/datasets/%s/download' % dataset_id
         yield Request(
             url=url,
@@ -50,3 +57,10 @@ class DatasetsSpider(scrapy.Spider):
         self.logger.info('Saving file to %s', file_path)
         with open(file_path, 'wb') as new_dataset_file:
             new_dataset_file.write(response.body)
+        unzip_folder = os.path.join(dirname, slug)
+        if not os.path.exists(unzip_folder):
+            os.makedirs(unzip_folder)
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall(unzip_folder)
+        os.remove(file_path)
+        self.query_next_dataset()
