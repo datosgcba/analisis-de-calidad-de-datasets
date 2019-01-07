b2cc5c59014b697027cef4ea6d8f7fe5fa9d490e
Ignacio Heredia <ignacio.nh@gmail.com>
2019-01-06 21:22:28 -0300

    Move helpers to helpers file and create a new cleaner_rules file

--- datasets/cleaner.py
+++ datasets/cleaner.py
@@ -1,52 +1,30 @@
 from data_cleaner import DataCleaner
 from os import path, makedirs
-from cleaner_rules import cleaner_rules
+from cleaner_rules import target_datasets
 
 
-def clean_and_save(input_path, output_path, rules):
+def makedir_if_doesnt_exist(output_path):
     output_folder = path.join(path.dirname(output_path))
     if not path.exists(output_folder):
         makedirs(output_folder)
-
-    dc = DataCleaner(input_path)
-    dc.clean(rules)
-    dc.df.set_index(dc.df.columns[0]).to_csv(
-        output_path,
-        encoding=dc.OUTPUT_ENCODING,
-        sep=dc.OUTPUT_SEPARATOR,
-        quotechar=dc.OUTPUT_QUOTECHAR
-    )
-
-
-def make_dataset_path(*paths):
-    return path.join(root_datasets_folder, *paths)
-
-
-def clean_dataset_path(original_dataset_path):
-    dirname, filename = path.split(original_dataset_path)
-    return path.join(dirname, 'clean', filename)
-
-
-def rules_for_dataset(dataset_path):
-    dataset_folder_path, dataset_filename = path.split(dataset_path)
-    dataset_root_folder, dataset_folder_name = path.split(dataset_folder_path)
-    dataset_path = path.join(dataset_folder_name, dataset_filename)
-    if dataset_path in cleaner_rules:
-        return cleaner_rules[dataset_path]
-    dirname = path.dirname(dataset_path)
-    if dirname in cleaner_rules:
-        return cleaner_rules[dirname]
-    raise Exception("No rules found for %s" % dataset_path)
 
 
 if __name__ == '__main__':
     root_datasets_folder = path.dirname(__file__)
 
-    target_datasets = [
-        make_dataset_path("obras-registradas", "obras-registradas.csv")
-    ]
+    for dataset in target_datasets:
+        csv_input = path.join(root_datasets_folder, dataset['input_path'])
+        csv_output = path.join(root_datasets_folder, dataset['output_path'])
+        dataset_rules = dataset['cleaner_rules']
+        parse_options = dataset.get('parse_options', {})
 
-    for csv_input in target_datasets:
-        csv_output = clean_dataset_path(csv_input)
-        rules = rules_for_dataset(csv_input)
-        clean_and_save(csv_input, csv_output, rules)
+        makedir_if_doesnt_exist(csv_output)
+
+        dc = DataCleaner(csv_input, **parse_options)
+        dc.clean(dataset_rules)
+        dc.df.set_index(dc.df.columns[0]).to_csv(
+            csv_output,
+            encoding=dc.OUTPUT_ENCODING,
+            sep=dc.OUTPUT_SEPARATOR,
+            quotechar=dc.OUTPUT_QUOTECHAR
+        )
